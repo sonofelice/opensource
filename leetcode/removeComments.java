5d84801f10d7ff21f21fa2e73d5f71aa220fc9a7
sonofelice <sonofelice_mm@163.com>
2015-09-24 16:22:13 +0800

    add new files—removeComments

---
+++ leetcode/removeComments.java
@@ -0,0 +1,55 @@
+package leetcode;
+
+public class removeComments{
+	    public  String removeCommentsXiaomi(String code){  
+	        StringBuilder sb = new StringBuilder();  
+	        int commentnum = 0;  
+	        boolean flag = false;  
+	        for (int i = 0; i < code.length(); i++){
+	            if(!flag){ //not quote
+	                if(code.charAt(i) == '\"'){   //quote 
+	                    sb.append(code.charAt(i));  
+	                    flag = true;  
+	                    continue;  
+	                }  
+	                else if(i+1 < code.length() && code.charAt(i) == '/' && code.charAt(i+1) == '/'){  
+	                    while(code.charAt(i) != '\n') i++;
+	                    i--;//防止删除"//"注释后面的换行符"
+	                    continue;  
+	                }else{  
+	                    if(commentnum == 0){  
+	                        if(i+1 < code.length() && code.charAt(i) == '/' && code.charAt(i+1) == '*'){  
+	                            commentnum ++;  
+	                            i ++;  
+	                            continue;  
+	                        }  
+	                    }else{  
+	                        if(i+1 < code.length() && code.charAt(i) == '*' && code.charAt(i+1) == '/'){  
+	                            commentnum --;  
+	                            i++;  
+	                            continue;  
+	                        }
+	                        if(i+1 < code.length() && code.charAt(i) == '/' && code.charAt(i+1) == '*'){  
+	                        	commentnum ++;  
+	                            i++;  
+	                            continue;  
+	                        }  
+	                    }  
+	                    if(commentnum == 0){	          
+	                        sb.append(code.charAt(i));  
+	                        continue;  
+	                    }  
+	                }  
+	            }else{
+	                if(code.charAt(i) == '\"' && code.charAt(i-1) != '\\'){  
+	                    sb.append(code.charAt(i));  
+	                    flag = false;  
+	                }else{  
+	                	//sb.append(code.charAt(i-1));
+	                    sb.append(code.charAt(i));  
+	                }  
+	            }  
+	        }  
+	        return sb.toString();  
+	    }  
+}
